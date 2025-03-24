102b4f84f73048accaf829e66bc6a7bda49bd2db
한규호 <luca.han@paymint.co.kr>
2025-03-24 17:55:32 +0900

    코드 리팩토링

--- src/main/java/com/example/springbootpractice/model/dto/WebtoonTop3Response.java
+++ src/main/java/com/example/springbootpractice/model/dto/WebtoonTop3Response.java
@@ -24,6 +24,19 @@ public record WebtoonTop3Response(
         WebtoonRatingType ratingType
     ) {
 
+        public static WebtoonCountInfoResponse from(WebtoonTop3Info top3Info) {
+            return WebtoonCountInfoResponse
+                .builder()
+                .seq(top3Info.webtoonSeq())
+                .count(top3Info.count())
+                .name(top3Info.name())
+                .author(top3Info.author())
+                .coin(top3Info.coin())
+                .openDate(top3Info.openDate())
+                .ratingType(top3Info.ratingType())
+                .build();
+        }
+
     }
 
     public static WebtoonTop3Response of(
@@ -31,28 +44,16 @@ public record WebtoonTop3Response(
         List<WebtoonTop3Info> dislikeTop3Info
     ) {
         return WebtoonTop3Response.builder()
-            .likeTop3List(likeTop3Info.stream()
-                .map(item -> WebtoonCountInfoResponse.builder()
-                    .seq(item.webtoonSeq())
-                    .count(item.count())
-                    .name(item.name())
-                    .author(item.author())
-                    .coin(item.coin())
-                    .openDate(item.openDate())
-                    .ratingType(item.ratingType())
-                    .build())
-                .toList())
-            .dislikeTop3List(dislikeTop3Info.stream()
-                .map(item -> WebtoonCountInfoResponse.builder()
-                    .seq(item.webtoonSeq())
-                    .count(item.count())
-                    .name(item.name())
-                    .author(item.author())
-                    .coin(item.coin())
-                    .openDate(item.openDate())
-                    .ratingType(item.ratingType())
-                    .build())
-                .toList())
+            .likeTop3List(
+                likeTop3Info.stream()
+                    .map(WebtoonCountInfoResponse::from)
+                    .toList()
+            )
+            .dislikeTop3List(
+                dislikeTop3Info.stream()
+                    .map(WebtoonCountInfoResponse::from)
+                    .toList()
+            )
             .build();
     }
 

3aa3032af68eb39a3cc20e5646ce088ebcba72c6
한규호 <luca.han@paymint.co.kr>
2025-03-24 18:03:55 +0900

    코드 리팩토링

--- src/main/java/com/example/springbootpractice/model/dto/SignUpRequest.java
+++ src/main/java/com/example/springbootpractice/model/dto/SignUpRequest.java
@@ -14,8 +14,8 @@ public record SignUpRequest(
 
     @NotBlank
     @Pattern(
-        regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])+[.][a-zA-Z]{2,3}$"
-        , message = "올바르지 않은 이메일 형식 입니다."
+        regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])+[.][a-zA-Z]{2,3}$",
+        message = "올바르지 않은 이메일 형식 입니다."
     )
     String email,
 

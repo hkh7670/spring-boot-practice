c97159c4a2c84f8deb6b417ad5138a73d1fec293
한규호 <luca.han@paymint.co.kr>
2025-03-24 17:37:53 +0900

    코드 리팩토링

--- src/main/java/com/example/springbootpractice/config/security/JwtTokenProvider.java
+++ src/main/java/com/example/springbootpractice/config/security/JwtTokenProvider.java
@@ -54,8 +54,11 @@ public class JwtTokenProvider {
     // JWT 토큰에서 인증 정보 조회
     public Authentication getAuthentication(String token) {
         UserDetails userDetails = userDetailsService.loadUserByUsername(this.getUserEmail(token));
-        return new UsernamePasswordAuthenticationToken(userDetails, "",
-            userDetails.getAuthorities());
+        return new UsernamePasswordAuthenticationToken(
+            userDetails,
+            "",
+            userDetails.getAuthorities()
+        );
     }
 
     // 토큰에서 회원 정보 추출

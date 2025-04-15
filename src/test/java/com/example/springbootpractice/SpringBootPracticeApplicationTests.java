9ed24157906d4138d4acda47fa67d681b47cf363
한규호 <hkh7670@gmail.com>
2025-04-15 15:32:30 +0900

    test code yml 파일 수정

--- src/test/java/com/example/springbootpractice/SpringBootPracticeApplicationTests.java
+++ src/test/java/com/example/springbootpractice/SpringBootPracticeApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.springbootpractice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // application-test.yml 사용
 class SpringBootPracticeApplicationTests {
 
     @Test

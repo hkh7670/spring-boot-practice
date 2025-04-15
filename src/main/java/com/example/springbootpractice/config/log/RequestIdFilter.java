f0344504019ce82bc45f5a690d5d8db56034068f
한규호 <luca.han@paymint.co.kr>
2025-04-15 15:07:07 +0900

    log에 thread id가 출력되도록 수정

--- src/main/java/com/example/springbootpractice/config/log/RequestIdFilter.java
+++ src/main/java/com/example/springbootpractice/config/log/RequestIdFilter.java
@@ -1,6 +1,7 @@
 package com.example.springbootpractice.config.log;
 
 import static com.example.springbootpractice.util.CommonConstants.REQUEST_ID_HEADER;
+import static com.example.springbootpractice.util.CommonConstants.THREAD_ID;
 import static com.example.springbootpractice.util.CustomStringUtils.hasNotText;
 
 import com.example.springbootpractice.util.CommonUtils;
@@ -25,6 +26,7 @@ public class RequestIdFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
         throws IOException, ServletException {
+        long startTime = System.currentTimeMillis(); // 요청 시작 시간
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
 
         // 클라이언트가 Request ID를 보내지 않으면 UUID 생성
@@ -32,12 +34,15 @@ public class RequestIdFilter implements Filter {
         if (hasNotText(requestId)) {
             requestId = CommonUtils.getRequestId();
         }
+        String threadId = Thread.currentThread().isVirtual()
+            ? "virtual-thread-" + Thread.currentThread().threadId()
+            : Thread.currentThread().getName();
 
-        long startTime = System.currentTimeMillis(); // 요청 시작 시간
         String method = httpServletRequest.getMethod();
         String uri = httpServletRequest.getRequestURI();
 
         try {
+            MDC.put(THREAD_ID, threadId);
             MDC.put(REQUEST_ID_HEADER, requestId); // MDC에 저장하여 로그에서 활용
             log.info("[{} {}] 요청 시작", method, uri);
             chain.doFilter(request, response);
--- src/main/java/com/example/springbootpractice/util/CommonConstants.java
+++ src/main/java/com/example/springbootpractice/util/CommonConstants.java
@@ -3,5 +3,6 @@ package com.example.springbootpractice.util;
 public class CommonConstants {
 
     public static final String REQUEST_ID_HEADER = "X-Request-ID";
+    public static final String THREAD_ID = "THREAD-ID";
 
 }

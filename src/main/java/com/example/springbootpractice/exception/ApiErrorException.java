8b4c047bcd7f83d8957757f3e3e1a12025900b87
한규호 <hkh7670@gmail.com>
2025-04-17 11:06:07 +0900

    ApiErrorException 클래스에 serialVersionUID 추가

--- src/main/java/com/example/springbootpractice/exception/ApiErrorException.java
+++ src/main/java/com/example/springbootpractice/exception/ApiErrorException.java
@@ -1,9 +1,14 @@
 package com.example.springbootpractice.exception;
 
+import java.io.Serial;
 import lombok.Getter;
 
 @Getter
 public class ApiErrorException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
 
     private final ErrorCode errorCode;
 
